6fadcdf0823e8b1ab36c78b3abc697061d37d4ff
Ain Mor <a.mor@memon.de>
2026-08-26 16:23:20 +0200

    Implement custom object and Pug views

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,20 +9,80 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+const CUSTOM_OBJECT_ID = process.env.CUSTOM_OBJECT_ID;
 
+console.log('Token loaded:', !!PRIVATE_APP_ACCESS);
+console.log('Custom Object ID:', CUSTOM_OBJECT_ID);
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const booksUrl =
+        `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_ID}`;
 
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const resp = await axios.get(booksUrl, {
+            headers,
+            params: {
+                properties: 'name,author,genre'
+            }
+        });
+
+        const data = resp.data.results;
+
+        res.render('homepage', {
+            title: 'Books | Integrating With HubSpot I Practicum',
+            records: data
+        });
+
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+        res.status(500).send('Unable to load Books from HubSpot.');
+    }
+});
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
-
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', {
+        title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+    });
+});
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const booksUrl =
+        `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_ID}`;
 
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    const newBook = {
+        properties: {
+            name: req.body.name,
+            author: req.body.author,
+            genre: req.body.genre
+        }
+    };
+
+    try {
+        await axios.post(booksUrl, newBook, { headers });
+
+        res.redirect('/');
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+        res.status(500).send('Unable to create Book in HubSpot.');
+    }
+});
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
 
